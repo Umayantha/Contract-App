32fe09ca342f3fa1217e653fd65aa344242cc9c7
Umayantha93 <umayantha93@gmail.com>
2024-01-24 08:48:04 +0530

    update date into table

--- resources/views/person/index.blade.php
+++ resources/views/person/index.blade.php
@@ -9,9 +9,25 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    @foreach ($people as $person)
-                        {{ $person->firstname }}
-                    @endforeach
+                    <table class="table-fixed border-separate border-spacing-6">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Phone</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($people as $person)
+                            <tr>
+                                <td>{{ $person->firstname }} {{ $person->lastname }}</td>
+                                <td>{{ $person->email }}</td>
+                                <td>{{ $person->phone }}</td>
+                            </tr>
+                            
+                        @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
